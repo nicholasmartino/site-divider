a3010b46ea6e599d8276c31e25db1d0d026a9fe0
Nicholas Martino <nicholasmartino@host212-99.vpn.ubc.ca>
2021-12-15 16:19:00 -0800

    Deployment

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,13 @@
-module.exports = {
-    devServer: {
-      host: '127.0.0.1',
-      port: 8080,
-      public: 'localhost:8080',
-    },
-  };
+// module.exports = {
+//     devServer: {
+//       host: '127.0.0.1',
+//       port: 8080,
+//       public: 'localhost:8080',
+//     },
+//   };
   
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/site-divider/'
+  : '/'
+}
